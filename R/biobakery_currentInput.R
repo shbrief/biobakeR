3871a19d5d094b61ff4a8c3ec41f7d62ef399a7e
Sehyun Oh <shbrief@hotmail.com>
2021-07-06 23:35:48 -0400

    update

--- R/biobakery_currentInput.R
+++ R/biobakery_currentInput.R
@@ -6,29 +6,33 @@
 #' @param accountEmail Email linked to Terra account
 #' @param billingProjectName Name of the billing project
 #' @param workspaceName Name of the workspace
-#' @param inputOnly Under the default (\code{TRUE}), the file path to the input
-#' files list and the paths to input files will be returned. If it's set to \code{FALSE},
-#' the whole method configuration will be returned.
+#' @param allConfig Under the default (\code{FALSE}), the file path to the input
+#' files list and the paths to input files will be returned. If it's set to
+#' \code{TRUE}, the whole method configuration will be returned.
 #'
 #' @export
-biobakery_currentInput <- function(accountEmail, billingProjectName, workspaceName,
-                                   inputOnly = TRUE) {
+biobakery_currentInput <- function(accountEmail,
+                                   billingProjectName,
+                                   workspaceName,
+                                   allConfig = FALSE) {
 
     parsed <- RunTerraWorkflow::currentInput(accountEmail,
                                              billingProjectName,
                                              workspaceName,
-                                             inputOnly = FALSE) # grab all the configuration info
+                                             inputOnly = FALSE)
 
     ## Return the whole method configuration
-    if (isFALSE(inputOnly)) {return(parsed)}
-
-    ## Return only the input part of the method configuration
-    res <- parsed$inputs$workflowMTX.inputRead1Files
-    if (nzchar(res)) {
-        res <- gsub("\"", "", res)
-        structure(list(inputListPath = res,
-                       inputFilePath = AnVIL::gsutil_cat(res)))
+    if (isFALSE(allConfig)) {
+        return(parsed)
     } else {
-        return("Input files are not provided.")
+        ## Return only the input part of the method configuration
+        res <- parsed$inputs$workflowMTX.inputRead1Files
+        if (nzchar(res)) {
+            res <- gsub("\"", "", res)
+            structure(list(inputListPath = res,
+                           inputFilePath = AnVIL::gsutil_cat(res)))
+        } else {
+            return("Input files are not provided.")
+        }
     }
 }
